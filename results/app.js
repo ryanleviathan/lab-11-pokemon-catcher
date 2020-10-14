fb41ea5e744d6c01b0726e480bf199b7b0fc91e4
ryanleviathan <ryan.m.carreras@gmail.com>
2020-10-14 15:30:40 -0700

    final commit, MVP for both lab days

--- results/app.js
+++ results/app.js
@@ -8,34 +8,127 @@ function getPokemonFromLocalStorage(key) {
 
 const pokemonResultsArray = getPokemonFromLocalStorage(POKEMON);
 
-const captureData = pokemonResultsArray.filter((item) => item.capture);
+// create new array
+const captureData = pokemonResultsArray
+    .filter((item) => item.encounter)
+    .map((item) => ({
+        name: item.pokemon,
+        captures: item.capture,
+        encounters: item.encounter
+    }));
+
+const pokemonLabels = captureData.map((item) => item.name);
+const pokemonEncounters = captureData.map((item) => item.encounters);
+const pokemonCaptures = captureData.map((item) => item.captures);
+
 console.log(captureData);
-const capturedPokemon = captureData.map((item) => {
-    item.pokemon,
-        item.capture;
+
+var ctx = document.getElementById('myChart');
+
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: pokemonLabels,
+        datasets: [{
+            label: '# of encounters',
+            data: pokemonEncounters,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
 });
-console.log(capturedPokemon);
 
+var ctx2 = document.getElementById('myChart2');
 
-
-
-
-const encounterData = pokemonResultsArray.filter((item) => item.encounter);
-console.log(encounterData);
-
-
-
-
-
-// const encounteredArray = pokemonResultsArray.map(element => {
-//     for (let i = 0; i < pokemonResultsArray.length; i++) {
-//         return pokemonResultsArray[i].encounter;
-//     }
-// });
-// console.log(encounteredArray);
-
-// const capturedArray = pokemonResultsArray.find(element => {
-
-// } element.capture);
-// console.log(capturedArray);
-
+var myChart2 = new Chart(ctx2, {
+    type: 'bar',
+    data: {
+        labels: pokemonLabels,
+        datasets: [{
+            label: '# of captures',
+            data: pokemonCaptures,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
